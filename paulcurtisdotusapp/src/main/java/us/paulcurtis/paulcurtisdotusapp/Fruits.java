befcc9ce41e271e229d22a332182b9222c9e8fd0
Paul Curtis <pcurtis5688@gmail.com>
2020-03-22 01:01:29 -0400

    v0.01 PaulCurtisDotUSApp Commit.412-716-0747 pcurtis5688@gmail.com http://paulcurtis.us

---
+++ paulcurtisdotusapp/src/main/java/us/paulcurtis/paulcurtisdotusapp/Fruits.java
@@ -0,0 +1,10 @@
+package us.paulcurtis.paulcurtisdotusapp;
+
+/* FRUITS CLASS*/
+public class Fruits {
+    public static void main(String[] args) {
+        System.out.println("I love eating fruits");
+        System.out.println("Mango is my favorite fruit");
+        System.out.println("I love apples as well");
+    }
+}
